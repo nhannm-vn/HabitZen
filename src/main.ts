3c384ea7092a2f78f6d54634a799e75a45d9ad73
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2026-05-30 14:13:51 +0700

    authen lock

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
     .setTitle('HabitZen API')
     .setDescription('API docs')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
